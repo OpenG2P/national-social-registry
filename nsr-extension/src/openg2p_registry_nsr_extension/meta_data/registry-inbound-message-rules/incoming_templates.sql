16e3d45d0086b6ec5f67f6b63451332fe3f8fe4a
tahzeer <tahzeer.ash@gmail.com>
2026-05-26 14:34:37 +0530

    G2P-4933 feat: update SQL seed data and templates for DCI and CRVSVC integration
    - Modified SQL seed data to reference new template file names for DCI and CRVSVC.
    - Added new JSON templates for dci_commons_response and dci_to_nsr_individual to enhance data processing.
    - Updated existing templates to improve data handling and ensure consistency across the integration process.

--- nsr-extension/src/openg2p_registry_nsr_extension/meta_data/registry-inbound-message-rules/incoming_templates.sql
+++ nsr-extension/src/openg2p_registry_nsr_extension/meta_data/registry-inbound-message-rules/incoming_templates.sql
@@ -1,6 +1,6 @@
 INSERT INTO "public"."incoming_templates" ("template_id","register_id","data_model_id","template_file_id","jsonld_expansion_required","created_at","updated_at") VALUES 
-('926c9745-f820-4f4f-a25a-077c6558a903','a0000000-0000-4000-8000-000000000001','DM1','9f7a6ef49d9f4d3f8b54f3aeac6bba70','FALSE','2026-05-12 08:41:39.962695',NULL),
-('dec9f1e7-cabe-4890-85f2-77131ba39fdc','a0000000-0000-4000-8000-000000000001','d3fdeb82-6f19-4aab-a3fb-12437dc4caff','095b602e21ab491892a94bc0f7d8cd93','FALSE','2026-05-19 05:16:55.540936',NULL);
+('926c9745-f820-4f4f-a25a-077c6558a903','a0000000-0000-4000-8000-000000000001','DM1','dci_to_nsr_individual.json.j2','FALSE','2026-05-12 08:41:39.962695',NULL),
+('dec9f1e7-cabe-4890-85f2-77131ba39fdc','a0000000-0000-4000-8000-000000000001','d3fdeb82-6f19-4aab-a3fb-12437dc4caff','crvsvc_to_nsr_individual.json.j2','FALSE','2026-05-19 05:16:55.540936',NULL);
 
--- DCI: templates/ingest/dci_to_nsr_individual.json.j2
--- CRVSVC: templates/ingest/crvsvc_to_nsr_individual.json.j2
+-- DCI: dci_to_nsr_individual.json.j2
+-- CRVSVC: crvsvc_to_nsr_individual.json.j2
