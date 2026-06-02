f5b1a5867c7c31ff0c9630822385905f0f3d08a5
vin0dkhichar <abbb61667@gmail.com>
2026-06-02 11:23:37 +0530

    G2P-4924 Seed AWE approval policies

--- nsr-extension/src/openg2p_registry_nsr_extension/meta_data/registry-configurations/g2p_input_mechanisms.sql
+++ nsr-extension/src/openg2p_registry_nsr_extension/meta_data/registry-configurations/g2p_input_mechanisms.sql
@@ -1,3 +1,4 @@
 INSERT INTO "public"."g2p_input_mechanisms" ("mechanism_id","register_id","mechanism_type","display_key") VALUES 
-('1','9055ab43-c85d-4833-bd00-ca657bb72644','INTAKE_FORM','Intake Form'),
-('2','a0000000-0000-4000-8000-000000000001','VERIFIABLE_CREDENTIAL','VC');
+('1','a0000000-0000-4000-8000-000000000001','INTAKE_FORM','Intake Form'),
+('2','a0000000-0000-4000-8000-000000000002','INTAKE_FORM','Intake Form'),
+('3','a0000000-0000-4000-8000-000000000001','VERIFIABLE_CREDENTIAL','VC');
